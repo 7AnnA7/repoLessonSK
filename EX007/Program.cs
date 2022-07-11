2593e65ac54b190e3d5d34472c37441a0374c414
AnnA <anna_elfimova@mail.ru>
2022-07-11 19:45:37 +0300

    EX 7

---
+++ EX007/Program.cs
@@ -0,0 +1,13 @@
+﻿// about masha
+Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower()=="маша")
+{
+Console.Write("УРА! Это же МАША!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.Write(username);
+}
